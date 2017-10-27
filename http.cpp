083ec73fe81dc28979bdd525187f37f4b5f9d5e8
Daniel Kamil Kozar <dkk089@gmail.com>
2017-10-27 23:56:14 +0200

    curl support

--- http.cpp
+++ http.cpp
@@ -9,11 +9,21 @@
 
 #include "http.h"
 #include <curl/curl.h>
+#include <cstdlib>
+#include <iostream>
 
 namespace {
 struct CurlInit {
   CurlInit() {
-    ::curl_global_init(CURL_GLOBAL_DEFAULT);
+    if(::curl_global_init(CURL_GLOBAL_DEFAULT)) {
+      std::cerr << "libcurl initialisation failed\n";
+      ::exit(1);
+    }
+    auto versinfo = ::curl_version_info(CURLVERSION_NOW);
+    if(!(versinfo->features & CURL_VERSION_SSL)) {
+      std::cerr << "Your libcurl doesn't support SSL\n";
+      ::exit(1);
+    }
   }
   ~CurlInit() {
     ::curl_global_cleanup();
