9bf20714a9a1146d7d557399a6f40984a35af9ad
Daniel Kamil Kozar <dkk089@gmail.com>
2020-08-11 01:01:36 +0200

    Add vcpkg's CMAKE_TOOLCHAIN_FILE to cmake invocation

--- workflow_build_windows.bat
+++ workflow_build_windows.bat
@@ -2,6 +2,6 @@ call "C:\Program Files (x86)\Microsoft Visual Studio\2019\Enterprise\VC\Auxiliar
 vcpkg install curl:x86-windows-static
 mkdir build
 cd build
-cmake .. -DCMAKE_BUILD_TYPE=MinSizeRel -DVCPKG_TARGET_TRIPLET=x86-windows-static -G Ninja
+cmake .. -DCMAKE_BUILD_TYPE=MinSizeRel -DVCPKG_TARGET_TRIPLET=x86-windows-static -DCMAKE_TOOLCHAIN_FILE=C:/vcpkg/scripts/buildsystems/vcpkg.cmake -G Ninja
 ninja
 7z a -t7z -m0=lzma -mx=9 -mfb=64 -md=32m -ms=on -sse dcue.7z dcue.exe
