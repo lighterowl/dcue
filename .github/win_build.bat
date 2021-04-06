bdb061a8b24947db7aafb367d17e8ffe9ad93de9
Daniel Kamil Kozar <dkk089@gmail.com>
2021-04-06 02:40:10 +0200

    curl is not needed on windows anymore

--- .github/win_build.bat
+++ .github/win_build.bat
@@ -1,8 +1,7 @@
 call "C:\Program Files (x86)\Microsoft Visual Studio\2019\Enterprise\VC\Auxiliary\Build\vcvars32.bat"
-vcpkg install curl:x86-windows-static
 mkdir build
 cd build
-cmake .. -DCMAKE_BUILD_TYPE=MinSizeRel -DVCPKG_TARGET_TRIPLET=x86-windows-static -DCMAKE_TOOLCHAIN_FILE=C:/vcpkg/scripts/buildsystems/vcpkg.cmake -G Ninja
+cmake .. -DCMAKE_BUILD_TYPE=MinSizeRel -G Ninja
 ninja
 7z a -t7z -m0=lzma -mx=9 -mfb=64 -md=32m -ms=on -sse dcue.7z dcue.exe
 certutil -hashfile dcue.7z SHA256
