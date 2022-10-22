a0436ec8523ffb7e49e11f7daac7289aae08e34b
Daniel Kamil Kozar <dkk089@gmail.com>
2022-10-22 22:45:43 +0200

    windows-latest uses MSVC 2022

--- .github/win_build.bat
+++ .github/win_build.bat
@@ -1,4 +1,4 @@
-call "C:\Program Files (x86)\Microsoft Visual Studio\2019\Enterprise\VC\Auxiliary\Build\vcvars32.bat"
+call "C:\Program Files\Microsoft Visual Studio\2022\Enterprise\VC\Auxiliary\Build\vcvars32.bat"
 mkdir build
 cd build
 cmake .. -DCMAKE_BUILD_TYPE=%1 -G Ninja
