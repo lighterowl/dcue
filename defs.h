a7f34bf8b5e07c8341fdfad3870cebf14a83fbf5
Daniel Kamil Kozar <dkk089@gmail.com>
2022-11-07 21:18:47 +0100

    Back to devel

--- defs.h
+++ defs.h
@@ -13,7 +13,7 @@
 #ifndef DCUE_DEFS_H
 #define DCUE_DEFS_H
 
-#define VERSION "1.5"
+#define VERSION "1.6dev"
 #define USER_AGENT "DCue/" VERSION " +https://github.com/xavery/dcue"
 #define COMMENT "DCue v" VERSION
 
